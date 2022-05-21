3806e16c2e8b29bf15d4b397af0daecf3536c033
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-05-21 14:14:14 +0100

    first

---
+++ MVVMFromMVC/Utilities/Box.swift
@@ -0,0 +1,29 @@
+
+
+import Foundation
+final class Box<T> {
+  //1
+  typealias Listener = (T) -> Void
+  var listener: Listener?
+  //2
+  var value: T {
+    didSet {
+      listener?(value)
+    }
+  }
+  //3
+  init(_ value: T) {
+    self.value = value
+  }
+  //4
+//  func bind(listener: Listener?) {
+//    self.listener = listener
+//    listener?(value)
+//  }
+  
+  
+  func bind(liste: ((T) -> Void )?)  {
+    self.listener = liste 
+    liste?(value)
+  }
+}
